432db8acf831e7465f22f7c4ee5ad63cca324907
Sai <Sai@DESKTOP-DUQIM9F>
2022-02-11 15:06:24 +0530

    by sarika

--- 21-10Feb-GitHub/src/DemoGitHub/DemoGit.java
+++ 21-10Feb-GitHub/src/DemoGitHub/DemoGit.java
@@ -2,10 +2,15 @@ package DemoGitHub;
 
 public class DemoGit {
 	
+	public void m1()
+	{
+		System.out.println("m1 method");
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Sarikaaaaaaaaaaa");
-		System.out.println("aaa");
+		System.out.println("Lilly");
 	}
 
 }
